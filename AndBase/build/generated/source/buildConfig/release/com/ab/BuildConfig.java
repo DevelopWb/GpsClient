4c33a8ea1d044561b7cfe5d25a7ad4c1746dbe90
wb <three6one@163.com>
2021-09-02 22:37:46 +0800

    link

--- AndBase/build/generated/source/buildConfig/release/com/ab/BuildConfig.java
+++ AndBase/build/generated/source/buildConfig/release/com/ab/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.ab";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 19;
-  public static final String VERSION_NAME = "";
+  public static final int VERSION_CODE = 113;
+  public static final String VERSION_NAME = "1.1.3";
 }
